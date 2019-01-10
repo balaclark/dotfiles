c822eb1fd33d392bbb124463597f893a811ff877
Bala Clark <balaclark@gmail.com>
2019-01-10 11:59:06 +0000

    mongorc prettier

--- mongorc.js
+++ mongorc.js
@@ -1,25 +1,31 @@
-DBCollection.prototype.read = function (id) {
-  return this.findOne({ _id: ObjectId(id) })
-}
+DBCollection.prototype.read = function(id) {
+  return this.findOne({ _id: ObjectId(id) });
+};
 
-DBCollection.prototype.delete = function (id) {
-  return this.remove({ _id: ObjectId(id) })
-}
+DBCollection.prototype.delete = function(id) {
+  return this.remove({ _id: ObjectId(id) });
+};
 
 function notablescan() {
-  var currentValue = db.getSiblingDB('admin').runCommand({ getParameter: 1, notablescan: 1 }).notablescan
+  var currentValue = db
+    .getSiblingDB("admin")
+    .runCommand({ getParameter: 1, notablescan: 1 }).notablescan;
 
-  printjson(db.getSiblingDB('admin').runCommand({ setParameter: 1, notablescan: !currentValue }))
+  printjson(
+    db
+      .getSiblingDB("admin")
+      .runCommand({ setParameter: 1, notablescan: !currentValue })
+  );
 }
 
 function clearUpTestDbs() {
-  var dbs = db.adminCommand('listDatabases').databases
-  dbs.forEach(function (database) {
+  var dbs = db.adminCommand("listDatabases").databases;
+  dbs.forEach(function(database) {
     if (database.name.match(/^test.+/)) {
-      print('Dropping DB: ' + database.name)
-      db.getSiblingDB(database.name).dropDatabase()
+      print("Dropping DB: " + database.name);
+      db.getSiblingDB(database.name).dropDatabase();
     }
-  })
+  });
 }
 
-clearUpTestDbs()
+clearUpTestDbs();
